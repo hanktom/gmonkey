56afa089a4fc322d32a1ad1ea204a1cd00e035a3
Hank Tom <hanktom@gmail.com>
2024-07-29 00:52:52 +0800

    hello

--- build/distributions/gtvmonkey.user.js
+++ build/distributions/gtvmonkey.user.js
@@ -9,6 +9,10 @@
 // @require     https://github.com/hanktom/gmonkey/tree/master/build/distributions/gtvmonkey.js
 // @require     https://github.com/hanktom/gmonkey/tree/master/build/distributions/gtvmonkey.user.js
 // @require     https://github.com/hanktom/gmonkey/tree/master/build/distributions/gtvmonkey.js.map
-// ==/UserScript==!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.gtvmonkey=t():e.gtvmonkey=t()}(this,(()=>{return e={524:function(e,t){var o,n;void 0===(n="function"==typeof(o=function(e){"use strict";return console.info("Testing"),e})?o.apply(t,[t]):o)||(e.exports=n)}},t={},function o(n){var r=t[n];if(void 0!==r)return r.exports;var p=t[n]={exports:{}};return e[n].call(p.exports,p,p.exports,o),p.exports}(524);var e,t}));
+// ==/UserScript==
+
+$(document).ready(function() {
+    console.log('Hello');
+}!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.gtvmonkey=t():e.gtvmonkey=t()}(this,(()=>{return e={524:function(e,t){var o,n;void 0===(n="function"==typeof(o=function(e){"use strict";return console.info("Testing"),e})?o.apply(t,[t]):o)||(e.exports=n)}},t={},function o(n){var r=t[n];if(void 0!==r)return r.exports;var p=t[n]={exports:{}};return e[n].call(p.exports,p,p.exports,o),p.exports}(524);var e,t}));
 //# sourceMappingURL=gtvmonkey.js.map
 main();
