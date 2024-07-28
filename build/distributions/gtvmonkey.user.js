72d60bc0e6ed2f37d7e07d9854867805695ea77d
Hank Tom <hanktom@gmail.com>
2024-07-28 23:09:17 +0800

    init

---
+++ build/distributions/gtvmonkey.user.js
@@ -0,0 +1,13 @@
+ // ==UserScript==
+// @name        Shopee scripts - KotlinJs
+// @namespace   Shopee Scripts
+// @grant       GM.xmlHttpRequest
+// @grant       GM.notification
+// @version     0.1
+// @author      Hank Tom
+// @description 7/28/2024, 9:43:07 PM
+// @require     file://C:\2024\gtvmonkey-scripts\build\distributions\gtvmonkey.js
+// @require     file://C:\2024\gtvmonkey-scripts\build\distributions\gtvmonkey.user.js
+// ==/UserScript==!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.gtvmonkey=o():e.gtvmonkey=o()}(this,(()=>{return e={524:function(e,o){var t,r;void 0===(r="function"==typeof(t=function(e){"use strict";return window.alert('Monkey say... "Hello World!"'),e})?t.apply(o,[o]):t)||(e.exports=r)}},o={},function t(r){var n=o[r];if(void 0!==n)return n.exports;var p=o[r]={exports:{}};return e[r].call(p.exports,p,p.exports,t),p.exports}(524);var e,o}));
+//# sourceMappingURL=gtvmonkey.js.map
+main();
