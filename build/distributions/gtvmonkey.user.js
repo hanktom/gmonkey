0f2b9afb3bafb2b84fff02477976c4d384d9ddcc
Hank Tom <hanktom@gmail.com>
2024-07-28 23:10:55 +0800

    url built

--- build/distributions/gtvmonkey.user.js
+++ build/distributions/gtvmonkey.user.js
@@ -6,8 +6,9 @@
 // @version     0.1
 // @author      Hank Tom
 // @description 7/28/2024, 9:43:07 PM
-// @require     file://C:\2024\gtvmonkey-scripts\build\distributions\gtvmonkey.js
-// @require     file://C:\2024\gtvmonkey-scripts\build\distributions\gtvmonkey.user.js
+// @require     https://github.com/hanktom/gmonkey/tree/master/build/distributions/gtvmonkey.js
+// @require     https://github.com/hanktom/gmonkey/tree/master/build/distributions/gtvmonkey.user.js
+// @require     https://github.com/hanktom/gmonkey/tree/master/build/distributions/gtvmonkey.js.map
 // ==/UserScript==!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.gtvmonkey=o():e.gtvmonkey=o()}(this,(()=>{return e={524:function(e,o){var t,r;void 0===(r="function"==typeof(t=function(e){"use strict";return window.alert('Monkey say... "Hello World!"'),e})?t.apply(o,[o]):t)||(e.exports=r)}},o={},function t(r){var n=o[r];if(void 0!==n)return n.exports;var p=o[r]={exports:{}};return e[r].call(p.exports,p,p.exports,t),p.exports}(524);var e,o}));
 //# sourceMappingURL=gtvmonkey.js.map
 main();
